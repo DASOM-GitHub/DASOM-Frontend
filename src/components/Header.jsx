4367632677276f55255a93b08e953d0141a2a8df
sunjung <sunjeong2201@gmail.com>
2024-08-01 22:55:56 +0900

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import "./Header.css";
 
-
 const Header = () => {
   
   return (
-    <div>
-    </div>
+    <header className="header">
+        
+      <div className="container">
+      <nav className="header-nav">
+        <ul>
+          <li><a href="#about">About</a> </li>
+          <li><a href="#Recurit">Recurit</a> </li>
+          <li><a href="#FAQ">FAQ</a> </li>
+        </ul>
+      </nav>
+      </div>
+    </header>
   );
 };
 
