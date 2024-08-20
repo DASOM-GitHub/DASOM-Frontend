71a83608c6afaf2967e2b52939cc9a309a481140
yewon_lee <dapang14@naver.com>
2024-08-21 00:50:44 +0900

    admin login connecting: 진행중

--- src/pages/AdminPage/Admin.jsx
+++ src/pages/AdminPage/Admin.jsx
@@ -1,41 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import "./Admin.css";
 import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios';
+
 
 
 const Admin = () => {
 
-        const [id, setId] = useState('');
+        const [email, setEmail] = useState('');
         const [password, setPassword] = useState('');
       
         const navigate = useNavigate();
-        // const [, setCookie] = useCookies(['refreshToken']);
       
 
-        //   const handleSubmit = async () => {
-        //       try {
-        //         const response = await axios.post('https://dmu-dasom.or.kr:8090/admin', {
-        //               id : id,
-        //               password : password
-        //         });
+          const handleSubmit = async () => {
+              try {
+                const response = await axios.post('https://dmu-dasom.or.kr/auth/login', {
+                      email : email,
+                      password : password
+                });
       
-        //         if (response.request.status === 200 ) {
-        //         console.log(response.status)
-        //           console.log('로그인 성공');
-        //            localStorage.setItem("accessToken", response.data.data.accessToken);
-        //            setCookie("refreshToken", response.data.data.refreshToken)
-        //            console.log(response.request.status)
-        //           navigate('/admin2');
-        //         }
-        //       } catch (error) {
-        //         if (error.response && error.response.status === 200) {
-        //           console.error('인증완료.');
-        //         } else {
-        //         alert("아이디 혹은 비밀번호가 틀렸습니다")
-        //           console.error('에러:', error);
-        //         }
-        //       }
-        //     };
+                if (response.request.status === 200 ) {
+                console.log(response.status)
+                  console.log('로그인 성공');
+                   localStorage.setItem("accessToken", response.data.data.accessToken);
+                   
+                   console.log(response.request.status)
+                  navigate('AapplySuccess');
+                }
+              } catch (error) {
+                if (error.response && error.response.status === 200) {
+                  console.error('인증완료.');
+                } else {
+                alert("아이디 혹은 비밀번호가 틀렸습니다")
+                  console.error('에러:', error);
+                }
+              }
+            };
   return (
    <div className='admin'>
     <div className='admin-login-form'>
@@ -43,10 +44,10 @@ const Admin = () => {
         <div className='admin-login-id'>
         <input
         type='email'
-        className="login-id"
-        placeholder='아이디'
-        value={id}
-        onChange={(e) => setId(e.target.value)}
+        className="login-email"
+        placeholder='이메일'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
         />
         </div>
         <div className='admin-login-pw'>
@@ -58,7 +59,7 @@ const Admin = () => {
         onChange={(e) => setPassword(e.target.value)}
         />
         </div>
-        <button className='login-button'>로그인 하기</button>
+        <button className='login-button' onClick={handleSubmit}>로그인 하기</button>
         </div>
     </div>
   );
