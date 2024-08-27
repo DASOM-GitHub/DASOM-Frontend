8b200989c4166b953db38226b2d8ac9180a439f3
DESKTOP-9E5KB98\user <b01023320838@gmail.com>
2024-08-28 02:41:03 +0900

    수정 사항: 초 단위 제거

--- src/pages/RecruitPage/Recruit.jsx
+++ src/pages/RecruitPage/Recruit.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+// import "./Recruit.css";
 import "./Recruit2.css";
+import Footer from "../../components/Footer";
 import axios from "axios";
 
 const Recruit = () => {
