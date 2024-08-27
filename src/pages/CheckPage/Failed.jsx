3ab1ea5ba323595ead2b0c2cd4bca87db7865846
yewon_lee <dapang14@naver.com>
2024-08-28 02:29:20 +0900

    합격 여부 페이지 css 수정중

--- src/pages/CheckPage/Failed.jsx
+++ src/pages/CheckPage/Failed.jsx
@@ -1,15 +1,24 @@
-import React from "react";
 import "./FinalPassed.css";
-
-
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Failed = () => {
+    const [name, setName] = useState(''); 
+    const location = useLocation();
+
+    useEffect(() => {
+        console.log('location.state:', location.state); 
+        if (location.state && location.state.applicantName) {
+            setName(location.state.applicantName);
+        }
+    }, [location.state]);
+
     return (
-        <div className="finalpassed">
-            <div className="finalpassed-box">       
-                <div className='finalpassed-title'>지원해 주셔서 감사합니다</div>
-                <div className='finalpassed-texts'>
-                    <p>아쉽지만 이번 DASOM 34기 지원에서 <span>탈락</span>하셨습니다.</p>
+        <div className="midpassed">
+            <div className="midpassed-box">       
+                <div className='midpassed-title'>{name}님, 지원해 주셔서 감사합니다</div>
+                <div className='midpassed-texts'>
+                    <p>{name}님은 아쉽지만 이번 DASOM 34기 지원에서 <span>불합격</span>하셨습니다.</p>
                     <p>비록 이번에는 함께 할 수 없게 되었지만</p>
                     <p>지원 과정에서 보여주신 열정과 노력에 깊은 감사를 드립니다.</p>
                     <p>앞으로의 여정과 새로운 도전에 행운이 가득하길 바라며</p>
--- src/pages/CheckPage/MidPassed.jsx
+++ src/pages/CheckPage/MidPassed.jsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './MidPassed.css';
 
 
 const MidPassed = () => {
+    const navigate = useNavigate();  
+    const handleBackClick = () => {
+        navigate('/main');  
+      };
+    
 
     const [url, setUrl] = useState();
     const [name, setName] = useState();
@@ -26,20 +32,38 @@ const MidPassed = () => {
     return (
         <div className='midpassed'>
             <div className='midpassed-box'>
-                <div className='midpassed-title'>🎉 {name}님, 축하드립니다 🎉</div>
+                <div className='midpassed-title'>🎉 <span>{name}님</span>, 축하드립니다 🎉</div>
                 <div className='midpassed-texts'>
                     <p>DASOM 33.5기 지원에서 <span>1차 합격</span> 하셨습니다.</p>
-                    <p>함께하게 될 미래를 기대하며</p>
-                    <p>면접을 통해 여러분의 이야기를 더 가까이에서 듣고자 합니다.</p>
-                    <p>아래 버튼을 눌러 면접을 예약해주세요.</p>
-                </div>
+                    <p>함께하게 될 미래를 기대하며
+                    면접을 통해 여러분의 이야기를 더 가까이에서 듣고자 합니다.</p>
+                    <br/>
+                    <p>33.5기 면접은 오프라인 방식으로 진행합니다.</p>
+                    <p>하단 예약하기 버튼을 눌러 가능한 시간대에 면접을 예약해주시면 됩니다.</p>
+                    <p>시간 선택은 선착순이며 다수의 지원자분들과 원활한 시간조율을 위한 불가피한 방안인 점 이해 바랍니다.</p>
+                    <p>하단 버튼을 눌러 면접을 예약해주세요.</p>
+                    <br/>
+                    </div>
+                    <div className='midpassed-text2'>
+                    <p><span>{name}님</span>과 만날 날을 고대하고 있겠습니다.</p>
+                    <p>이후 궁금하신 점은 <span>회장 남호성: 010-6225-6023</span> 을 통해 연락부탁드립니다.</p>
+                    <p>감사합니다. 🙂</p>
+                    </div>
+                    
+       
+
+                
                 <button
                     className='midpassed-reservebtn'
                     onClick={handleReserveClick}>
                     면접 예약하기
                 </button>
-            </div>
+
+                <div className='back-box' onClick={handleBackClick}>  
+                    <div className='back'> 홈으로 → </div>
         </div>
+      </div>
+            </div>
     );
 };
 
